b2187268f088820926d956c2a7e044f34fc998ba
Kondratev AS <KondratevAS@pg-spb.ru>
2024-11-25 13:54:31 +0300

    построил траекторию

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Временной интервал
+t = np.linspace(0, 10, 500)
+
+# Определение функций для сферических координат
+r = 1 + 0.3 * np.sin(20 * t)
+phi = t
+theta = -np.exp(-0.001 * t) + np.pi / 2
+
+# Преобразование сферических координат в декартовы
+x = r * np.sin(theta) * np.cos(phi)
+y = r * np.sin(theta) * np.sin(phi)
+z = r * np.cos(theta)
+
+# Настройка графика
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim([-2, 2])
+ax.set_ylim([-2, 2])
+ax.set_zlim([-2, 2])
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+ax.set_title('Animated Trajectory')
+
+# Создание точки для анимации
+point, = ax.plot([], [], [], 'ro')  # 'ro' означает красная точка
+trajectory_line, = ax.plot([], [], [], 'b-')  # 'b-' означает синяя линия
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Инициализация функции
+def init():
+    point.set_data([], [])
+    point.set_3d_properties([])
+    trajectory_line.set_data([], [])
+    trajectory_line.set_3d_properties([])
+    return point, trajectory_line
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Функция обновления для анимации
+def update(frame):
+    # Обновляем позицию точки
+    point.set_data(x[frame:frame + 1], y[frame:frame + 1])  # Передаем массив длиной 1
+    point.set_3d_properties(z[frame:frame + 1])  # Передаем массив длиной 1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Обновляем данные для линии
+    trajectory_line.set_data(x[:frame + 1], y[:frame + 1])  # Все данные до текущего кадра
+    trajectory_line.set_3d_properties(z[:frame + 1])  # Все данные до текущего кадра
+
+    return point, trajectory_line
+
+
+# Создание анимации
+ani = FuncAnimation(fig, update, frames=len(t), init_func=init, blit=True, interval=20)
+
+# Сохранение анимации в формате GIF
+ani.save('trajectory_animation.gif', writer='pillow', fps=30)
+
+plt.show()
